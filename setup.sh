ed45d528ad4ac7bd7828c51967521e73257a825c
Arjay Sitoy <sitoy.arjay@gmail.com>
2025-09-13 00:30:23 +0700

    feat: graphrag scaffold using python

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt install -y \
+    build-essential \
+    cmake \
+    git \
+    libgtk2.0-dev \
+    pkg-config \
+    libavcodec-dev \
+    libavformat-dev \
+    libswscale-dev \
+    python3 \
+    python3-pip \
+    python3-venv \
+    python-is-python3
+
+
+python -m venv .venv
+
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install graphrag
